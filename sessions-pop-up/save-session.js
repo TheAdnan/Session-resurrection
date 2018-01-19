d33079660d861d7970bf09142b71be7b744eda38
TheAdnan <adnankicin92@gmail.com>
2018-01-19 16:45:36 +0100

    :tulip: downloads permission added, save in resources as json

--- sessions-pop-up/save-session.js
+++ sessions-pop-up/save-session.js
@@ -1,1 +1,33 @@
-console.log("fool me once - shame on you, fool me twice - fiddle dee do")
+var tabsLogged = [];
+var jsonSession = "";
+
+function logTabs(tabs) {
+    var jsonSession = "{";
+    for (let tab of tabs) {
+        jsonSession += ("'" + tab.url + "',");
+        tabsLogged.push(tab.url);
+    }
+    jsonSession += "}";
+    saveToJSON(jsonSession, 'session-' + Date.now() + '.json');
+}
+
+function onError(error) {
+    console.log(`Error: ${error}`);
+}
+
+function saveToJSON(text, name) {
+    var a = document.createElement("a");
+    var file = new Blob([text], {type: 'text/plain'});
+    a.href = URL.createObjectURL(file);
+    a.download = "../saved-sessions/" + name;
+	browser.downloads.download({
+      url : a.href,
+      saveAs : true
+	});
+}
+
+
+var querying = browser.tabs.query({
+    currentWindow: true
+});
+querying.then(logTabs, onError);
