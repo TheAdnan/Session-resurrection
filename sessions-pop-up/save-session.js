468b66ee35c5e373c0ef3e510a5fdd3b8dd02272
TheAdnan <adnankicin92@gmail.com>
2018-01-19 16:08:03 +0100

    jquery added, background script for gathering tab data

--- sessions-pop-up/save-session.js
+++ sessions-pop-up/save-session.js
@@ -1,12 +1,1 @@
-function logTabs(tabs) {
-  for (let tab of tabs) {
-    console.log(tab.url);
-  }
-}
-
-function onError(error) {
-  console.log(`Error: ${error}`);
-}
-
-var querying = browser.tabs.query({currentWindow: true});
-querying.then(logTabs, onError);
+console.log("fool me once - shame on you, fool me twice - fiddle dee do")
