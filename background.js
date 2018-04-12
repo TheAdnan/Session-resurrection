5b82cd51679eca7dc2dbee321c64f97ac518d815
TheAdnan <adnankicin92@gmail.com>
2018-04-12 10:30:13 +0200

    v 1.3 :crab: :cat2:

--- background.js
+++ background.js
@@ -6,15 +6,12 @@ function onError(error) {
   return;
 }
 
-var windowID = browser.windows.getLastFocused({"windowTypes": ["normal"]});
-const winID = windowID.then(logTabs, onError);
-
-
 browser.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "restore_session" ) {
     	request.urls.forEach(function(url){
-    		winID.then(function(result){
+            var winID = browser.windows.getLastFocused({"windowTypes": ["normal"]}).then(logTabs, onError);
+            winID.then(function(result){
     			var createTab = browser.tabs.create({"windowId": result, "url": url});
     		});
     	});
